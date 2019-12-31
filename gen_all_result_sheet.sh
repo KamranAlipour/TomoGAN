9e2ac850c598989bc1e42f6d43597346181aaa66
KamranAlipour <kalipour@eng.ucsd.edu>
2019-12-31 06:50:17 +0000

    resize the images from 1024 to 512 in generating final results to be consistent with other models.

--- gen_all_result_sheet.sh
+++ gen_all_result_sheet.sh
@@ -1,1 +1,1 @@
-python3 generate_result_sheet.py -gpu 0 -resfolder training_results/final_result_sheets -weights training_results/s1s2s3_training_res/train_cropped_s1_s2_s3_n10_n15/test-last-model.h5
+python3 generate_result_sheet.py -gpu 0 -resfolder training_results/final_result_sheets -weights training_results/s1s2s3_training_res/train_cropped_s1_s2_s3_n10_n15/test-last-model.h5 -inputdim 512
